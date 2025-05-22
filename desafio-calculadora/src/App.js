9a70e2a7b0ec1692a4bd570738d005f0ea3248b1
José Bento <joseluizbpj@gmail.com>
2025-05-21 23:52:00 -0300

    primeiro commit

---
+++ desafio-calculadora/src/App.js
@@ -0,0 +1,53 @@
+import Button from './components/Button';
+import Input from './components/Input';
+
+import { Container, Content, Row} from './styles';
+import { useState } from 'react';
+
+const App = () => {
+  const [currentNumber, setCurrentNumber  ] = useState('0');
+  
+  const handle
+
+  const handleOnClear = () =>{
+    setCurrentNumber('0')
+  };
+
+  const handleAddNumber = (number) => {
+    setCurrentNumber(prev => `${prev === '0' ? '' : prev}${number}`)
+  }
+
+  return (
+    <Container>
+      <Content>
+        <Input value={currentNumber}/>
+        <Row>
+            <Button label={"x"} onClick={() => handleAddNumber('')}/>
+            <Button label={"/"} onClick={() => handleAddNumber('')}/>
+            <Button label={"C"} onClick={handleOnClear}/>
+            <Button label={"X"} onClick={() => handleAddNumber('')}/>
+          </Row>
+        <Row>
+            <Button label={7} onClick={() => handleAddNumber('7')}/>
+            <Button label={8} onClick={() => handleAddNumber('8')}/>
+            <Button label={9} onClick={() => handleAddNumber('9')}/>
+            <Button label={"-"} onClick={() => handleAddNumber('-')}/>
+          </Row>
+        <Row>
+            <Button label={4} onClick={() => handleAddNumber('4')}/>
+            <Button label={5} onClick={() => handleAddNumber('5')}/>
+            <Button label={6} onClick={() => handleAddNumber('6')}/>
+            <Button label={"+"} onClick={() => handleAddNumber('+')}/>
+          </Row>
+          <Row>
+            <Button label={1} onClick={() => handleAddNumber('1')}/>
+            <Button label={2} onClick={() => handleAddNumber('2')}/>
+            <Button label={3} onClick={() => handleAddNumber('3')}/>
+            <Button label={"="} onClick={() => handleAddNumber('=')}/>
+          </Row>
+      </Content>
+    </Container >
+  );
+}
+
+export default App;
